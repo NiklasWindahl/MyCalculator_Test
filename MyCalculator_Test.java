9a4f52e40f16b9484e1e10e2e4b2f8d063d63ab7
Niklas Windahl <nikles90@hotmail.com>
2018-11-26 10:07:43 +0100

    Nya Test filen

--- MyCalculator_Test.java
+++ MyCalculator_Test.java
@@ -1,4 +1,24 @@
 public class MyCalculator_Test {
+  // java skapar en konsturktor, en grundläggande konsturktor;
+  private static MyCalculator calc = new MyCalculator();
 
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+
+  private static boolean test_StartValueOfFirstNumber(){
+    double temp = calc.getFirstNumber();
+    if (0 == temp){
+      return true;
+    }
+    return false;
+  }
+  public static void main(String[] args) {
+    if(test_StartValueOfFirstNumber());{
+      System.out.println("test_StartValueOfFirstNumber " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      }
+      System.out.println("test_StartValueOfFirstNumber " + ANSI_RED + "FAILED"+ ANSI_RESET);
+    }
 
 }
