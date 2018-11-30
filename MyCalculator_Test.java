69efa98842e0176f99752bb4bf45a381874ccf00
Niklas Windahl <nikles90@hotmail.com>
2018-11-30 10:04:09 +0100

    nya-version

--- MyCalculator_Test.java
+++ MyCalculator_Test.java
@@ -4,6 +4,7 @@ public class MyCalculator_Test {
 
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[0;34m";
     public static final String ANSI_RESET = "\u001B[0m";
 
 
@@ -41,26 +42,26 @@ public class MyCalculator_Test {
 
   public static void main(String[] args) {
     if(test_StartValueOfFirstNumber()){
-      System.out.println("test_StartValueOfFirstNumber " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      System.out.println(ANSI_BLUE + "test_StartValueOfFirstNumber:  " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
     }else{
-      System.out.println("test_StartValueOfFirstNumber " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      System.out.println("test_StartValueOfFirstNumber:  " + ANSI_RED + "FAILED"+ ANSI_RESET);
     }
 
     if(test_StartValueOfSecondNumber()){
-      System.out.println("test_StartValueOfSecondNumber " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      System.out.println(ANSI_BLUE + "test_StartValueOfSecondNumber: " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
     }else{
-      System.out.println("test_StartValueOfSecondNumber " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      System.out.println("test_StartValueOfSecondNumber: " + ANSI_RED + "FAILED"+ ANSI_RESET);
     }
 
     if(test_StartValueOfOperation()){
-      System.out.println("test_StartValueOfOperation " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      System.out.println(ANSI_BLUE + "test_StartValueOfOperation:    " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
     }else{
-      System.out.println("test_StartValueOfOperation " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      System.out.println("test_StartValueOfOperation:    " + ANSI_RED + "FAILED"+ ANSI_RESET);
     }
     if(test_StartValueOfAnswer()){
-      System.out.println("test_StartValueOfAnswer " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
+      System.out.println(ANSI_BLUE + "test_StartValueOfAnswer:       " + ANSI_GREEN + "PASSED"+ ANSI_RESET);
     }else{
-      System.out.println("test_StartValueOfAnswer " + ANSI_RED + "FAILED"+ ANSI_RESET);
+      System.out.println("test_StartValueOfAnswer:       " + ANSI_RED + "FAILED"+ ANSI_RESET);
     }
   }
 }
